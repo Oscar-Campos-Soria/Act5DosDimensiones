5d3153e63291c36b79268df79eec0af7e8934839
FALKON <campossoriao1@gmail.com>
2024-03-09 01:22:33 +0100

    Agregar archivos de proyecto.

---
+++ Act5DosDimensiones/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Act5DosDimensiones
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMatriz = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMatriz
+            // 
+            this.dgvMatriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatriz.Location = new System.Drawing.Point(12, 83);
+            this.dgvMatriz.Name = "dgvMatriz";
+            this.dgvMatriz.RowTemplate.Height = 25;
+            this.dgvMatriz.Size = new System.Drawing.Size(547, 173);
+            this.dgvMatriz.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Matriz";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(12, 27);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(110, 23);
+            this.txtNumero.TabIndex = 5;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(128, 27);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 29);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar Matriz";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ingresa un numero";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvMatriz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMatriz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Act5DosDimensiones/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act5DosDimensiones
+{
+
+    public partial class Form1 : Form
+    {
+        private const int Filas = 5;
+        private const int Columnas = 5;
+        private int[,] matriz;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerarMatrizAleatoria();
+        }
+
+        private void GenerarMatrizAleatoria()
+        {
+            matriz = new int[Filas, Columnas];
+            Random rnd = new Random();
+
+            for (int i = 0; i < Filas; i++)
+            {
+                for (int j = 0; j < Columnas; j++)
+                {
+                    matriz[i, j] = rnd.Next(1, 101); // Números aleatorios entre 1 y 100
+                }
+            }
+
+            MostrarMatriz();
+        }
+
+        private void MostrarMatriz()
+        {
+            dgvMatriz.Rows.Clear();
+            dgvMatriz.Columns.Clear();
+
+            // Configurar la cuadrícula
+            dgvMatriz.ColumnCount = Columnas;
+
+            for (int i = 0; i < Filas; i++)
+            {
+                dgvMatriz.Rows.Add();
+                for (int j = 0; j < Columnas; j++)
+                {
+                    dgvMatriz.Rows[i].Cells[j].Value = matriz[i, j];
+                }
+            }
+        }
+
+
+
+
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+
+            int numeroBuscado;
+            if (int.TryParse(txtNumero.Text, out numeroBuscado))
+            {
+                bool encontrado = false;
+                for (int i = 0; i < Filas; i++)
+                {
+                    for (int j = 0; j < Columnas; j++)
+                    {
+                        if (matriz[i, j] == numeroBuscado)
+                        {
+                            MessageBox.Show($"El número {numeroBuscado} fue encontrado en la posición ({i + 1}, {j + 1}).");
+                            encontrado = true;
+                            break;
+                        }
+                    }
+                    if (encontrado)
+                        break;
+                }
+                if (!encontrado)
+                    MessageBox.Show($"El número {numeroBuscado} no fue encontrado en la matriz.");
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número entero válido.");
+            }
+
+        }
+    }
+
+}
